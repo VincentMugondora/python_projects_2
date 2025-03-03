87f70e4eab2f8395b73eddabbef4bcedc755d14f
VincentMugondora <vinmugondora@gmail.com>
2025-03-03 18:38:47 +0200

    built a simple word replacement function

---
+++ email_sender.py
@@ -0,0 +1,3 @@
+# go over to uor gmail account and setup 2 factor authentication
+# genarate app password
+# create a function to send the email
---
+++ word_replace.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "hi guys l am vincent mugondora hi, hi, hi, hi"
+    word_to_be_replaced = input("Enter word to be replaced: ")
+    word_replacement = input("Enter word replacement: ")
+    print(str.replace(word_to_be_replaced, word_replacement))
+
+replace_word()
