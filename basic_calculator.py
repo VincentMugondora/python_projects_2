bfc78c69e1578fd6299ffd1d8ce498e75b21587b
VincentMugondora <vinmugondora@gmail.com>
2025-03-04 08:28:34 +0200

    starting today with email sender

---
+++ basic_calculator.py
@@ -0,0 +1,11 @@
+# define the functions needed: add, sub, mul, div
+# print options to the user
+# ask for values
+# call the functions
+# while loop to continue the program until the user wants to exit
+
+# ================================================================
+
+def add(a, b):
+    answer = a + b
+    print(str(a)+ ""
--- email_sender.py
+++ email_sender.py
@@ -1,3 +1,9 @@
 # go over to uor gmail account and setup 2 factor authentication
 # genarate app password
 # create a function to send the email
+
+
+from email.message import EmailMessage
+
+email_sender = "vincent@uncommon.org"
+email_password = "xbly qdyy asmv fcqk"
