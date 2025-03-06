33831488474fde2c9c769ca0f73a87c0755ff30f
VincentMugondora <vinmugondora@gmail.com>
2025-03-06 08:24:11 +0200

    updated

---
+++ testing.py
@@ -0,0 +1,10 @@
+# year = int(input("Enter the year: "))
+# def leap_year(year):
+    
+#     if year % 4 == 0:
+#         print(f"{year} is a leap year")
+#     else:
+#         print(f"{year} is not a leap year")
+
+# leap_year(year)
+
